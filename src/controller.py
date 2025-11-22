bc9484a8cead327d7dd4b6aa5d94a39c73cde945
treysont1 <treysonthelusma1@gmail.com>
2025-11-21 22:46:44 -0500

    Created player projectile class and sprite group

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,7 @@
 import pygame
 from src.player import Player
 from src.exit import Exit
+from src.player_projectile import Player_Projectile
 
 
 class Controller:
@@ -11,29 +12,39 @@ class Controller:
         self.width, self.height = pygame.display.get_window_size()
         self.player = Player(self.width // 2, self.height //2, (50, 50))
         self.exit = Exit(self.width - 15, 15)
+        self.player_bullets = pygame.sprite.Group()
 
     def mainloop(self):
         run = True
         while run == True:
 
+            self.screen.fill("black")
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN and self.exit.exit_rect.collidepoint(event.pos):
                     run = False
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    print("shoot")        #must write shoot function
+                    position = pygame.mouse.get_pos()
+                    shot = Player_Projectile(position[0], position[1])
+                    self.player_bullets.add(shot)
+                    print("shoot")
+            
+            # print(shot)
+            self.player_bullets.draw(self.screen)
+
+            self.player_bullets.update()
 
             # Movement Function  
             keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_LEFT] and self.player.player_hitbox.x > 0:
+            if keys[pygame.K_LEFT] and self.player.hitbox.x > 0:
                 self.player.left()
 
-            if keys[pygame.K_RIGHT] and self.player.player_hitbox.right < self.width:
+            if keys[pygame.K_RIGHT] and self.player.hitbox.right < self.width:
                 self.player.right()
+            
         
-          
-            self.screen.fill("black")
-            self.screen.blit(self.player.player_model, self.player.player_hitbox)
+            self.screen.blit(self.player.model, self.player.hitbox)
             self.screen.blit(self.exit.exit_button, self.exit.exit_rect)
 
             pygame.display.flip()
--- src/player.py
+++ src/player.py
@@ -4,13 +4,17 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y, size, img = "assets/fighter_jet.png"):
         super().__init__()
-        self.image = pygame.image.load(img)
-        self.player_model = pygame.transform.scale(self.image, size)
-        self.player_hitbox = self.player_model.get_rect()
-        self.player_hitbox.center = (x, y)
+        self.original_image = pygame.image.load(img)
+        self.model = pygame.transform.scale(self.original_image, size)
+        self.hitbox = self.model.get_rect()
+        self.hitbox.center = (x, y)
         
     def left(self):
-        self.player_hitbox.x -= 3
+        self.hitbox.x -= 3
 
     def right(self):
-        self.player_hitbox.x += 3
+        self.hitbox.x += 3
+    
+
+
+
