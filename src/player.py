e292204226573a32ea99c9602c0bf5355bf4dba6
treysont1 <treysonthelusma1@gmail.com>
2025-12-04 20:45:22 -0500

    Give player image attribute

--- src/player.py
+++ src/player.py
@@ -5,8 +5,8 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, size, img = "assets/fighter_jet.png"):
         super().__init__()
         self.original_image = pygame.image.load(img)
-        self.model = pygame.transform.scale(self.original_image, size)
-        self.rect = self.model.get_rect()
+        self.image = pygame.transform.scale(self.original_image, size)
+        self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         
     def left(self, dt):
